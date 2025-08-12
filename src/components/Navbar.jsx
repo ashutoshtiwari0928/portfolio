314f0612691116dcba7abfa8491f34c65e5c7d66
Ashutosh Tiwari <ashutoshtiwari200128@gmail.com>
2025-08-12 18:51:42 +0530

    Navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="text-xl font-bold">MyLogo</div>
+          <div className="text-xl font-bold">Welcome to my portfolio!</div>
 
           {/* Desktop Links */}
           <div className="hidden md:flex space-x-6">
