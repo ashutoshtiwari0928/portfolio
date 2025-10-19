5d99f34064c9bb493329b32133764c5dfbe57477
Ashutosh Tiwari <ashutoshtiwari200128@gmail.com>
2025-10-19 12:21:57 +0530

    Changed hero section layout

--- src/components/HeroV2.jsx
+++ src/components/HeroV2.jsx
@@ -5,42 +5,62 @@ import { skills } from "../info/Skills";
 import { profile } from "../info/Profile";
 export default function Hero() {
   return (
-    <div
-      id="hero"
-      className="flex flex-col lg:flex-row h-auto lg:h-screen text-white px-6 md:px-12 lg:px-20 py-5 bg-gray-950"
-    >
-      {/* ===== Left Section ===== */}
-      <div className="flex flex-col lg:w-2/3 w-full text-center lg:text-left justify-center items-center lg:items-start space-y-6">
-        <section className="flex flex-col justify-center items-center lg:items-start">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-stretch-condensed">
-            Hi, I'm <span className="text-indigo-500">Ashutosh Tiwari</span>
-          </h1>
+    <div className="bg-gray-950 text-white h-auto lg:h-screen md:px-12 lg:px-20  py-15">
+      <div id="hero" className="flex flex-col lg:flex-row  ">
+        {/* ===== Left Section ===== */}
+        <div className="flex flex-col lg:w-2/3 w-full text-center lg:text-left justify-center items-center lg:items-start space-y-6">
+          <section className="flex flex-col justify-center items-center lg:items-start">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-stretch-condensed">
+              Hi, I'm <span className="text-indigo-500">Ashutosh Tiwari</span>
+            </h1>
 
-          <p className="mt-4 text-sm md:text-base lg:text-lg text-gray-300 font-mono max-w-2xl">
-            I architect and deliver robust, enterprise-grade applications —
-            ensuring scalability, maintainability, and reliability across the
-            full stack, with end-to-end automation via CI/CD pipelines, cloud
-            platforms, and modern engineering practices.
-          </p>
+            <p className="mt-4 text-sm md:text-base lg:text-lg text-gray-300 font-mono max-w-2xl">
+              I architect and deliver robust, enterprise-grade applications —
+              ensuring scalability, maintainability, and reliability across the
+              full stack, with end-to-end automation via CI/CD pipelines, cloud
+              platforms, and modern engineering practices.
+            </p>
 
-          <div className="flex justify-center lg:justify-start mt-6">
-            <a
-              href={resume}
-              download="AshutoshTiwari.pdf"
-              className="flex bg-gray-900 px-5 py-3 space-0.5 rounded-full"
-            >
-              <button className=" hover:text-indigo-600 transition shadow-lg">
-                Download Resume!
-              </button>
-            </a>
-          </div>
-        </section>
+            <div className="flex justify-center lg:justify-start mt-6">
+              <a
+                href={resume}
+                download="AshutoshTiwari.pdf"
+                className="flex bg-gray-900 px-5 py-3 space-0.5 rounded-full"
+              >
+                <button className=" hover:text-indigo-600 transition shadow-lg">
+                  Download Resume!
+                </button>
+              </a>
+            </div>
+          </section>
+        </div>
 
-        <p className="font-mono text-sm md:text-base text-center lg:text-left pt-6">
+        {/* ===== Right Section ===== */}
+        <div className="flex flex-col lg:w-1/3 w-full items-center justify-center mt-10 lg:mt-0 space-y-6">
+          <img
+            src={dp}
+            alt="Profile pic"
+            className="w-24 md:w-32 lg:w-48 mx-auto shadow-lg rounded-full border-2 border-gray-700"
+          />
+          <nav className="flex flex-wrap justify-center gap-5 bg-gray-900 shadow-lg rounded-full px-5 py-3">
+            {profile.map((item) => (
+              <a
+                href={item.href}
+                target="_blank"
+                className="text-gray-400 hover:text-indigo-600 transition"
+                title={item.title}
+              >
+                {<item.icon size={24} />}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+      {/* ===== Skills Section ===== */}
+      <div>
+        <p className="font-mono text-sm md:text-base text-center lg:text-left py-6">
           Tools and Skills:
         </p>
-
-        {/* ===== Skills Section ===== */}
         <section
           id="skills"
           className="flex flex-wrap justify-center lg:justify-start gap-3"
@@ -58,27 +78,6 @@ export default function Hero() {
             </div>
           ))}
         </section>
-      </div>
-
-      {/* ===== Right Section ===== */}
-      <div className="flex flex-col lg:w-1/3 w-full items-center justify-center mt-10 lg:mt-0 space-y-6">
-        <img
-          src={dp}
-          alt="Profile pic"
-          className="w-24 md:w-32 lg:w-48 mx-auto shadow-lg rounded-full border-2 border-gray-700"
-        />
-        <nav className="flex flex-wrap justify-center gap-5 bg-gray-900 shadow-lg rounded-full px-5 py-3">
-          {profile.map((item) => (
-            <a
-              href={item.href}
-              target="_blank"
-              className="text-gray-400 hover:text-indigo-600 transition"
-              title={item.title}
-            >
-              {<item.icon size={24} />}
-            </a>
-          ))}
-        </nav>
       </div>
     </div>
   );
